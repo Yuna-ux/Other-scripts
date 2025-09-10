e99bbde463dd142fd4edb5e090f38ab48da97ca7
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-09-10 17:04:34 -0300

    Update AnimationsString.lua

--- AnimationsString.lua
+++ AnimationsString.lua
@@ -1,1 +1,8 @@
+function getGitHubRawUrl(user, repo, branch, filePath)
+    return string.format("https://raw.githubusercontent.com/%s/%s/%s/%s",
+        user, repo, branch, filePath)
+end
 
+local url = getGitHubRawUrl("Yuna-ux", "Other-scripts", "main", "AnimationsTable.lua")
+
+return loadstring(game:HttpGet(url))
