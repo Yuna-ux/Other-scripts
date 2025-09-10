014cc87734fdf64088ef1c3f788b279cb9d2df54
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-09-10 17:08:40 -0300

    Create AnimationsTable.lua

---
+++ AnimationsTable.lua
@@ -0,0 +1,31 @@
+local animations = {
+    Bomb = Instance.new("Animation"),
+    Anvil = Instance.new("Animation"),
+    Forward = Instance.new("Animation"),
+    DeathRush = Instance.new("Animation"),
+    Rotate = Instance.new("Animation"),
+    Whirlwind = Instance.new("Animation"),
+    Dash = Instance.new("Animation"),
+    Dash2 = Instance.new("Animation"),
+    Jump = Instance.new("Animation"),
+    Slash = Instance.new("Animation"),
+    RapidSlash1 = Instance.new("Animation"),
+    RapidSlash2 = Instance.new("Animation"),
+    Cyclone = Instance.new("Animation")
+}
+
+animations.Bomb.AnimationId = "rbxassetid://16144846625"
+animations.Anvil.AnimationId = "rbxassetid://15636048652"
+animations.Forward.AnimationId = "rbxassetid://15561810697"
+animations.DeathRush.AnimationId = "rbxassetid://15775758181"
+animations.Rotate.AnimationId = "rbxassetid://16102682904"
+animations.Whirlwind.AnimationId = "rbxassetid://188242481"
+animations.Dash.AnimationId = "rbxassetid://16299510063"
+animations.Dash2.AnimationId = "rbxassetid://15436359788"
+animations.Jump.AnimationId = "rbxassetid://16102806530"
+animations.Slash.AnimationId = "rbxassetid://16102426718"
+animations.RapidSlash1.AnimationId = "rbxassetid://16102532028"
+animations.RapidSlash2.AnimationId = "rbxassetid://16102535685"
+animations.Cyclone.AnimationId = "rbxassetid://15437009238"
+
+return animations
